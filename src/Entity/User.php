549323efed195684d752c31e45e447934a323d37
Messali <lanymale33@gmail.com>
2021-09-14 20:55:42 +0200

    add assert

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use ShypBundle\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
