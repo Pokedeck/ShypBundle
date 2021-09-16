83d2ab6f5303b4c73a22f0fb51ea236716721e9b
adrienpignon <apignon@formation-lac.com>
2021-09-16 15:57:03 +0200

    fix

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -7,10 +7,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- * @ORM\InheritanceType("SINGLE_TABLE")
- */
 class Product
 {
     /**
