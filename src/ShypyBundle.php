89c0d1baa17d1640a2c2eacc48886fd29fe9b504
adrienpignon <apignon@formation-lac.com>
2021-09-16 15:29:23 +0200

    fix

--- src/ShypyBundle.php
+++ src/ShypyBundle.php
@@ -3,29 +3,9 @@
 namespace ShypyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ShypyBundle extends Bundle
 {
-  /**
-   * {@inheritDoc}
-   */
-  public function build(ContainerBuilder $container)
-  {
-      parent::build($container);
-
-      // With Yaml configuration format
-      $container->addCompilerPass(DoctrineOrmMappingsPass::createYamlMappingDriver(array(
-          realpath(__DIR__ . '/Resources/config/doctrine-mapping') => 'AppBundle\Model'
-      )));
-
-      // With annotation configuration format
-      $container->addCompilerPass(DoctrineOrmMappingsPass::createAnnotationMappingDriver(array(
-          'AppBundle\Model'
-      )));
-  }
-
   public function getPath(): string
   {
     return \dirname(__DIR__);
