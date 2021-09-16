24dcdb3f6807cacadceb705ae902320c13c9ec89
adrienpignon <apignon@formation-lac.com>
2021-09-16 12:47:09 +0200

    fix test

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -8,7 +8,8 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\MappedSuperclass
+ * @ORM\Entity
+ * @InheritanceType("SINGLE_TABLE")
  */
 class Product
 {
