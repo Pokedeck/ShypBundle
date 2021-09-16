5d898fb2a3b539732350d9e2f93daec24538e570
adrienpignon <apignon@formation-lac.com>
2021-09-16 15:48:19 +0200

    fix

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CartRepository::class)
- * @InheritanceType("SINGLE_TABLE")
+ * @ORM\InheritanceType("SINGLE_TABLE")
  */
 class Cart
 {
